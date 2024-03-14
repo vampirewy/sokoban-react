dee0f34b0f0371af08ed43544bb0d487d69fe271
vampirewy <haveaballmyball@hotmail.com>
2024-03-14 15:02:36 +0800

    feat:添加编辑区基本布局

--- src/views/Edit.tsx
+++ src/views/Edit.tsx
@@ -1,3 +1,13 @@
 export default function EditView() {
-  return <div>编辑地图</div>;
+  return (
+    <div>
+      <div className="flex">
+        <div className=" w-4/6 bg-pink-400">
+          <div>地图编辑区</div>
+        </div>
+        <div>数据展示区</div>
+      </div>
+      <div>元素选择区</div>
+    </div>
+  );
 }
