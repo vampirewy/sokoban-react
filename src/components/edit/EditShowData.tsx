5c7c8d30f8fb5dd868a11e7d8918ca7515545bde
vampirewy <haveaballmyball@hotmail.com>
2024-03-21 16:23:11 +0800

    feat:添加数据展示区

--- src/components/edit/EditShowData.tsx
+++ src/components/edit/EditShowData.tsx
@@ -1,8 +1,41 @@
+import { useEditCargo } from "@/composables/edit/editCargo";
+import { useEditMap } from "@/composables/edit/editMap";
+import { useEditPlayer } from "@/composables/edit/editPlayer";
+import { useEditTarget } from "@/composables/edit/editTarget";
+import { LevelGameData } from "@/game/gameData";
+import { ChangeEvent, useMemo } from "react";
+
 export default function EditShowDataView() {
+  const { storeMap } = useEditMap();
+  const { storePlayer } = useEditPlayer();
+  const { storeCargos } = useEditCargo();
+  const { storeTargets } = useEditTarget();
+
+  const data = useMemo<LevelGameData>(() => {
+    return {
+      map: storeMap,
+      player: storePlayer,
+      cargos: storeCargos,
+      targets: storeTargets,
+    };
+  }, [storeMap, storeCargos, storePlayer, storeTargets]);
+
+  function handleChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    console.log(e.target.value);
+  }
+
   return (
     <div>
       <h3>数据展示区</h3>
-      <textarea name="" id="" cols={30} rows={10} className="border border-black"></textarea>
+      <textarea
+        name=""
+        id=""
+        cols={30}
+        rows={10}
+        className="border border-black"
+        value={JSON.stringify(data)}
+        onChange={(e) => handleChange(e)}
+      ></textarea>
     </div>
   );
 }
